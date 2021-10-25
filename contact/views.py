3e55892e03fa60cac038f54509d0308552c24491
gwenjo <jo.gwen@hotmail.com>
2021-10-25 20:16:51 +0000

    added contact page

--- contact/views.py
+++ contact/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """ A view to return the contact page """
+
+    return render(request, 'contact/contact.html')
