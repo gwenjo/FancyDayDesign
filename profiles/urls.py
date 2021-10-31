40c98df779e1730ca9f88da0ae1af742d13e8208
gwenjo <jo.gwen@hotmail.com>
2021-10-31 23:20:53 +0000

    modified profile page and added edit profile and order history

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.profile, name='profile'),
-    path('order_history/<order_number>', views.order_history, name='order_history'),
+    path('edit_profile/', views.edit_profile, name='edit_profile'),
+    path('order_history/', views.order_history, name='order_history'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,44 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 
 from checkout.models import Order
 
+
+@login_required
 def profile(request):
     """ Display the user's profile. """
+
+    template = 'profiles/profile.html'
+    context = {
+        'on_profile_page': True
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def order_history(request):
+    """ Display the user's order history. """
+
+    profile = get_object_or_404(UserProfile, user=request.user)
+    orders = profile.orders.all()
+
+    template = 'profiles/order_history.html'
+    context = {
+        'orders': orders,
+        'on_profile_page': True,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def edit_profile(request):
+    """ Display the user's edit profile details form. """
+
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
@@ -15,32 +46,15 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
+        else:
+            messages.error(request, 'Update failed. Please ensure the form is valid.')
+    else:
+        form = UserProfileForm(instance=profile)
 
-    form = UserProfileForm(instance=profile)
-    orders = profile.orders.all()
-
-    template = 'profiles/profile.html'
+    template = 'profiles/edit_profile.html'
     context = {
         'form': form,
-        'orders': orders,
-        'on_profile_page': True
-    }
-
-    return render(request, template, context)
-
- 
-def order_history(request, order_number):
-    order = get_object_or_404(Order, order_number=order_number)
-
-    messages.info(request, (
-        f'This is a past confirmation for order number {order_number}. '
-        'A confirmation email was sent on the order date.'
-    ))
-
-    template = 'checkout/checkout_success.html'
-    context = {
-        'order': order,
-        'from_profile': True,
+        'on_profile_page': True,
     }
 
     return render(request, template, context)
